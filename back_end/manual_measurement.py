4462bf875a7ad62d6edc05fc79ef848dd4d61f69
tminnich <tminnich@unomaha.edu>
2025-04-20 17:33:30 -0500

    Finish manual_measurement.py

--- back_end/manual_measurement.py
+++ back_end/manual_measurement.py
@@ -9,16 +9,17 @@ Usage:
 
 @note The marker size must be provided in millimeters (mm).
 """
-
 import cv2 as cv
 import sys
 import two_marker_detect
+from demo_tool import MM_IN_RATIO
 from pathlib import Path
 
 def main():
     path = Path(sys.argv[1])
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
+        print("Error")
         print(__doc__)
         exit()
     if not path.exists():
@@ -27,7 +28,7 @@ def main():
 
     marker_size_mm = sys.argv[2]
     window_name = path.name
-    marker_coords, window_coords = []
+    marker_coords = window_coords = []
 
     image = cv.imread(path, cv.IMREAD_COLOR_RGB)
     cv.imshow(window_name, image)
@@ -43,6 +44,20 @@ def main():
 
     if len(window_coords) != 4:
         raise ValueError("too many corner coordinates for window")
+    
+    scale_px = two_marker_detect.get_scale(marker_coords)
+    t_coord_x, t_coord_y, b_coord_x, b_coord_y = two_marker_detect.get_diff_two_markers_px(window_coords, "TLBR")
+
+    h_px = abs(t_coord_x - b_coord_x)
+    w_px = abs(t_coord_y - b_coord_y)
+
+    scale_mm = marker_size_mm / scale_px
+
+    h_in = (h_px * scale_mm) / MM_IN_RATIO
+    w_in = (w_px * scale_mm) / MM_IN_RATIO
+
+    print(f"Width: {h_in:.2f} in")
+    print(F"Height: {w_in:.2f} in")
 
 def click_handler(event, x, y, _, params: tuple[list, list]):
     """Mouse click event handler. Only reacts to left mouse clicks."""
