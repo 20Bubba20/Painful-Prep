39bf46aa6f87545d7fe42d8ab5a3de7e56ee3e8a
tminnich <tminnich@unomaha.edu>
2025-05-07 12:08:27 -0500

    Add more test cases for two_marker_detect

--- back_end/tests/test_two_marker_detect.py
+++ back_end/tests/test_two_marker_detect.py
@@ -1,3 +1,5 @@
+'To run tests, go to back_end/tests directory, and run pytest from there.'
+
 import sys
 import os
 import pytest
@@ -6,7 +8,6 @@ import numpy as np
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import two_marker_detect
-
 
 @pytest.mark.parametrize(
         "marker_corners, expected",
@@ -25,7 +26,14 @@ def test_get_scale(marker_corners, expected):
         "corner_coords, orientation, expected",
         [
             (np.array([[0, 0], [1, 101], [100, 100], [101, 1]]), "TLBR", (0, 0, 100, 100)),
-            (np.array([[0, 0], [1, 101], [100, 100], [101, 1]]), "TLBR", (0, 0, 100, 100))
+            (np.array([[0, 0], [1, 101], [100, 100], [101, 1]]), "TRBL", (101, 1, 1, 101)),
+            (np.array([[101, 1], [0, 0], [1, 101], [100, 100]]), "TLBR", (0, 0, 100, 100)),
+            (np.array(
+                [[0, 0], [1, 101], [100, 100], [101, 1],
+                 [200, 200], [201, 301], [300, 300], [301, 200]]), "TLBR", (0, 0, 300, 300)),
+            (np.array(
+                [[1, 200], [0, 300], [100, 200], [101, 300],
+                 [300, 0], [200, 0], [201, 100], [301, 100]]), "TRBL", (300, 0, 0, 300))
         ]
 )
 def test_get_diff_two_markers_px(corner_coords, orientation, expected):
